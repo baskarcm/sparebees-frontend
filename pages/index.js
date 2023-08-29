41ea89bbf12cb8d1854947fe991e38356a6f18fc
baskar <baskarmca.php@gmail.com>
2023-08-29 10:49:04 +0530

    api config updated

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import PageHead from "../src/components/Helpers/PageHead";
 import Home from "./../src/components/Home/index";
 
 export default function HomePage({ data }) {
+  data = data.data;
+  // console.log(data);
   const { seoSetting } = data;
   return (
     <>
       <PageHead
-        title={`${seoSetting.seo_title}`}
+        // title={`${seoSetting.seo_title}`}
         metaDes={seoSetting.seo_description}
       />
       <Home homepageData={data} />
@@ -15,7 +17,8 @@ export default function HomePage({ data }) {
 }
 export async function getServerSideProps() {
   // Fetch data from external API
-  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/`);
-  // const data = await res.json();
-  return { props: { data: {} } };
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/`);
+  const data = await res.json();
+  console.log(data);
+  return { props: { data: {data} } };
 }
